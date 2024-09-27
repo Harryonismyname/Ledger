347ca0b26781ab2945cd98781dd8925eb8337b3c
Harryon <Harryonismyname@users.noreply.github.com>
2024-09-27 12:50:38 -0500

    Cleaned up App.tsx to remove unneccesary divs

--- ledger.client/src/App.tsx
+++ ledger.client/src/App.tsx
@@ -7,19 +7,13 @@ import Container from 'react-bootstrap/Container';
 function App() {
     const currentScreen = useCurrentScreenContext();
 
-    const contents = 
-    <Container fluid>
-            <Navbar />
-            <p>{currentScreen.value}
-            </p>
-    </Container>
-
     return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+        <Container fluid>
+            <Navbar />
+            <Container fluid>
+                {currentScreen.value}
+            </Container>
+        </Container>
     );
 }
 
