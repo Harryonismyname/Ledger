88e4d635d4868e623412bf8437d889f76ea7b30d
Harryon <Harryonismyname@users.noreply.github.com>
2024-10-23 14:29:56 -0500

    Pulled Bootstrap out of Main.tsx

--- ledger.client/src/main.tsx
+++ ledger.client/src/main.tsx
@@ -2,26 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { CurrentScreenProvider } from './currentScreenContext.tsx';
 import App from './App.tsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import Container from 'react-bootstrap/Container';
+import {Container} from '@mui/material';
 
 const root = document.getElementById('root');
 root!.className = "container-fluid";
 createRoot(root!).render(
     <StrictMode>
-        <Container fluid>
-            <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"></script>
-
-            <script
-                src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
-                ></script>
-
-            <script
-                src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
-                ></script>
-
-            <script>var Alert = ReactBootstrap.Alert;</script>
+        <Container>
             <CurrentScreenProvider>
                 <App />
             </CurrentScreenProvider>
